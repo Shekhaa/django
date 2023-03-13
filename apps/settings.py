2cddbe792b9c96a0d8983b698857cb1e30f5ffdb
Shekhaa <98873401+Shekhaa@users.noreply.github.com>
2023-03-13 01:23:14 -0700

    Update settings.py

--- apps/settings.py
+++ apps/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-dmj+devy9^tny5++gn#=06%@e!l2u^0_adgeligz@9pctdc5_d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versal.app']
 
 
 # Application definition
